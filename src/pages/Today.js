5a355281c4b7aed1729f1832aa16f532ddc86a05
Yoann Fleury <yoann.fleury@yahoo.com>
2019-07-11 00:07:09 +0200

    feat(today): add loading view

--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,41 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/templates/Layout";
 import ConferenceHall from "../services/ConferenceHall";
 import Card from "../components/molecules/Card";
 
 export default function Today() {
+  const [loading, setLoading] = useState(true);
+  const [talks, setTalks] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       const data = await ConferenceHall.getData();
-      console.log(data);
+      setTalks(data);
+      setLoading(false);
     }
 
     fetchData();
-  });
-
-  const conferences = [
-    {
-      id: "1",
-      title: "Managenement d'une équipe sportive",
-      speaker: "Guy Fournier",
-      room: "A"
-    },
-    {
-      id: "2",
-      title: "Symfony 4 - le framework PHP bâti pour grandir avec vos projets",
-      speaker: "Nicolas Grekas",
-      room: "A"
-    }
-  ];
+  }, []);
 
   return (
-    <Layout title="Programme">
-      {conferences.map(conference => (
-        <Card
-          key={conference.id}
-          to={`/talks/${conference.id}`}
-          conference={conference}
-        />
+    <Layout loading={loading} title="Programme">
+      {talks.map(talk => (
+        <Card key={talk.id} to={`/talks/${talk.id}`} conference={talk} />
       ))}
     </Layout>
   );
