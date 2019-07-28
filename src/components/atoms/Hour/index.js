52a04b460792173d72146e78b745d2babffb5d4e
Nicolas Torion <nicolas.torion@bearstudio.fr>
2019-07-28 21:07:47 +0200

    Add grouping talks by hours on Today and Favorite pages

--- src/components/atoms/Hour/index.js
+++ src/components/atoms/Hour/index.js
@@ -3,5 +3,7 @@ import styled from "styled-components";
 export default styled.div`
   color: ${props => props.theme.colors.secondary};
   font-family: ${props => props.theme.fonts.title};
-  font-size: 15px;
+  font-size: 1.3rem;
+  margin-top: 1rem;
+  font-weight: bold;
 `;
--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Layout from "../components/templates/Layout";
 import Card from "../components/molecules/Card";
 import { useTalks } from "../hooks/useTalks";
+import { groupBy } from 'lodash';
+import Hour from "../components/atoms/Hour";
 
 export default function Today() {
   const [talks, loading] = useTalks();
 
+  const talksGroupedByHour = groupBy(talks, 'hour');
+
   return (
     <Layout loading={loading} title="Programme">
-      {talks.map(talk => (
-        <Card key={talk.id} to={`/talks/${talk.id}`} conference={talk} />
+      {Object.keys(talksGroupedByHour).map((hour) => (
+          <Fragment key={hour}>
+            <Hour>{hour}</Hour>
+            {talksGroupedByHour[hour].map((talk) => (
+                <Card key={talk.id} to={`/talks/${talk.id}`} conference={talk} />
+            ))}
+          </Fragment>
       ))}
     </Layout>
   );
